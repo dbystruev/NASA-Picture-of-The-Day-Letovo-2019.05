380d6792378fe6fc7df8099b97d6185a2f50c6b7
Denis Bystruev <dbystruev@me.com>
2019-05-25 15:08:24 +0300

    Initial Commit

---
+++ NASA Picture of The Day/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NASA Picture of The Day
+//
+//  Created by Denis Bystruev on 25/05/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
